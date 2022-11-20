97632d9b4d0cf5acc4758f027fb987d19f91a96c
keza681 <lindakeza19@gmail.com>
2022-11-20 12:40:26 +0200

    Add update in the file

--- src/components/ManagPostedJobs.js
+++ src/components/ManagPostedJobs.js
@@ -8,10 +8,10 @@ export default function ManagPostedJobs() {
         <Table aria-label='simple table'>
             <TableHead>
                 <TableRow>
-                    <TableCell>Job Title</TableCell>
-                    <TableCell>Date</TableCell>
-                    <TableCell>View Job Detail</TableCell>
-                    <TableCell>View All Users</TableCell>
+                    <TableCell >Job Title</TableCell>
+                    <TableCell align='center'>Date</TableCell>
+                    <TableCell align='center'>Job Detail</TableCell>
+                    <TableCell align='center'>Candidates</TableCell>
                 </TableRow>
             </TableHead>
 
@@ -22,9 +22,9 @@ export default function ManagPostedJobs() {
                     sx={{'&:last-child td, &:last-child': { border:0 }}}>
 
                     <TableCell>{row.job_title}</TableCell>
-                    <TableCell>{row.date}</TableCell>
-                    <TableCell>{row.job_detail}</TableCell>
-                    <TableCell>{row.email}</TableCell>
+                    <TableCell align='center'>{row.date}</TableCell>
+                    <TableCell align='center' color='primary'>{row.job_detail}</TableCell>
+                    <TableCell align='center'>{row.candidates}</TableCell>
                     </TableRow>
                 ))}
             </TableBody>
@@ -35,73 +35,73 @@ export default function ManagPostedJobs() {
 }
 
 const TableData = [{
-    "job_title": 1,
-    "date": "Karon",
-    "job_detail": "Sissel",
-    "email": "ksissel0@time.com",
+    "job_title": "Senior Back-end developer",
+    "date": "Sep 12th, 2021",
+    "job_detail": "View",
+    "candidates": "View",
     "gender": "Female",
     "ip_address": "68.54.224.213"
 }, {
-    "job_title": 2,
-    "date": "Noella",
-    "job_detail": "Paudin",
-    "email": "npaudin1@cafepress.com",
+    "job_title": "Middle DevOps engineer",
+    "date": "Dec 12th, 2021",
+    "job_detail": "View",
+    "candidates": "View",
     "gender": "Female",
     "ip_address": "32.251.148.67"
 }, {
-    "job_title": 3,
-    "date": "Alisha",
-    "job_detail": "Eke",
-    "email": "aeke2@google.cn",
+    "job_title": "Junior Java developer",
+    "date": "Jan 12th, 2021",
+    "job_detail": "View",
+    "candidates": "View",
     "gender": "Agender",
     "ip_address": "91.186.214.106"
 }, {
-    "job_title": 4,
-    "date": "Broddie",
-    "job_detail": "Loverock",
-    "email": "bloverock3@weebly.com",
+    "job_title": "Junior Software engineer",
+    "date": "Mar 12th, 2021",
+    "job_detail": "View",
+    "candidates": "View",
     "gender": "Polygender",
     "ip_address": "89.198.156.40"
 }, {
-    "job_title": 5,
-    "date": "Markos",
-    "job_detail": "Dykins",
-    "email": "mdykins4@scribd.com",
+    "job_title": "Middle Platform engineer",
+    "date": "Oct 12th, 2021",
+    "job_detail": "View",
+    "candidates": "View",
     "gender": "Male",
     "ip_address": "212.149.239.163"
 }, {
-    "job_title": 6,
-    "date": "Deny",
-    "job_detail": "Berk",
-    "email": "dberk5@alexa.com",
+    "job_title": "Senior Reliability engineer",
+    "date": "Dec 12th, 2021",
+    "job_detail": "View",
+    "candidates": "View",
     "gender": "Female",
     "ip_address": "135.182.203.152"
 }, {
-    "job_title": 7,
-    "date": "Carlita",
-    "job_detail": "Sporrij",
-    "email": "csporrij6@rambler.ru",
+    "job_title": "Middle Cloud system engineer",
+    "date": "May 12th, 2021",
+    "job_detail": "View",
+    "candidates": "View",
     "gender": "Female",
     "ip_address": "125.108.107.126"
 }, {
-    "job_title": 8,
-    "date": "Charlton",
-    "job_detail": "Triggs",
-    "email": "ctriggs7@umich.edu",
+    "job_title": "Junior Software engineer",
+    "date": "Aug 12th, 2021",
+    "job_detail": "View",
+    "candidates": "View",
     "gender": "Male",
     "ip_address": "250.20.182.42"
 }, {
-    "job_title": 9,
-    "date": "Eddie",
-    "job_detail": "Illyes",
-    "email": "eillyes8@domainmarket.com",
+    "job_title": "Senior Cloud/software developer",
+    "date": "Jun 12th, 2021",
+    "job_detail": "View",
+    "candidates": "View",
     "gender": "Female",
     "ip_address": "134.182.221.23"
 }, {
-    "job_title": 10,
-    "date": "Fanchette",
-    "job_detail": "Whitehouse",
-    "email": "fwhitehouse9@alexa.com",
+    "job_title": "Senior Web developer",
+    "date": "Jul 12th, 2021",
+    "job_detail": "View",
+    "candidates": "View",
     "gender": "Female",
     "ip_address": "211.8.50.110"
 }]
