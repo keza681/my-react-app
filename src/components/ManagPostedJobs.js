d927db06ad537e0e5d04b86ab29d12eebef26a07
keza681 <lindakeza19@gmail.com>
2022-11-21 12:13:47 +0200

    Add update to the project

--- src/components/ManagPostedJobs.js
+++ src/components/ManagPostedJobs.js
@@ -1,7 +1,45 @@
 import React from 'react';
 import { TableContainer, Table, TableBody, TableRow, TableCell, Paper, TableHead, } from '@mui/material';
+// import Backdrop from '@mui/material/Backdrop';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+// import Fade from '@mui/material/Fade';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
 
 export default function ManagPostedJobs() {
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+    <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
+
     return (
         <div>
     <TableContainer component={Paper}>
@@ -12,6 +50,9 @@ export default function ManagPostedJobs() {
                     <TableCell align='center'>Date</TableCell>
                     <TableCell align='center'>Job Detail</TableCell>
                     <TableCell align='center'>Candidates</TableCell>
+                    <TableCell align='center'>Candidates</TableCell>
+                    <TableCell align='center'>Candidates</TableCell>
+
                 </TableRow>
             </TableHead>
 
@@ -24,7 +65,9 @@ export default function ManagPostedJobs() {
                     <TableCell>{row.job_title}</TableCell>
                     <TableCell align='center'>{row.date}</TableCell>
                     <TableCell align='center' color='primary'>{row.job_detail}</TableCell>
-                    <TableCell align='center'>{row.candidates}</TableCell>
+                    <TableCell 
+                    align='center'><Button onClick={handleOpen}>{row.candidates}</Button>
+                    </TableCell>
                     </TableRow>
                 ))}
             </TableBody>
